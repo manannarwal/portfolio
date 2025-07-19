fb7812dae5884e56173e3eb9e1726552479157eb
nikhila koneru <nikiko1002@gmail.com>
2025-07-19 12:28:18 -0400

    typescript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
-import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+ "./src/**/*.{js,ts,jsx,tsx,mdx}", ],
   theme: {
     extend: {
       fontFamily: {
